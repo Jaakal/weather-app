80011441e3bc9ddce186370e18b49095335c5a2a
Jaakal <jaak.kivinukk@gmail.com>
2020-04-16 00:23:40 +0300

    Small errors again

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 /* eslint-enable import/no-unresolved */
 
 // It is handy to not have those transformations while we developing
